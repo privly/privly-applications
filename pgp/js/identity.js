53c4de1ff392eb4ded76959f791c0a3bea3debf7
Trevor Bramwell <trevor@bramwell.net>
2014-04-16 22:15:36 -0700

    Add methods to PersonaId.
    sign, verify, bundle, verifyPayload, and extractPubkey all added for
    dealing with the payload to/from a directory provider.
    
    Test also updated with reference to PersonaId.

--- pgp/js/identity.js
+++ pgp/js/identity.js
@@ -1,29 +1,125 @@
-/*
- * Generate a Backed Identity Assertion
- */
+/**
+ * @fileOverview A set of functions for dealing with Directory Provider
+ * payloads and their fields.
+ *
+ * The payload from/to a DP follows this structure:
+ * {
+ *   pgp: A PGP public key signed by a Persona secret key. This is also
+ *        referred to as just the 'signature'.
+ *   bia: A backed identity assertion, which inherently contains the
+ *        public key of the secret key used to generate the 'pgp' field.
+ * }
+ *
+ * pgp - contains the results of 'sign'
+ * bia - contains the results of 'bundle'
+ **/
 var jwcrypto = require('/lib/jwcrypto');
 require('/lib/algs/ds');
 
-AUDIENCE = "https://privlyalpha.org:443"
-EXPIRATION_LIMIT = (60 * 60 * 24 * 30); // 1 Month
-
 jwcrypto.addEntropy("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
 
-/*
- * Takes a User Certificate (identity cert), a secretkey, and a
- * callback.
- *
- * The secret key is the user's not root certificate signer's
- */
-var generateBackedIdentityAssertion = function(cert, secretkey, cb) {
-    var ISSUED_AT = new Date();
-    var EXPIRES_AT = new Date(new Date().valueOf() + EXPIRATION_LIMIT);
-    jwcrypto.assertion.sign({},
-      {expiresAt: EXPIRES_AT, issuedAt: ISSUED_AT,
-       audience: AUDIENCE},
-       secretkey,
-      function(err, assertion) {
-        cb(err, jwcrypto.cert.bundle(cert, assertion));
-      }
-    );
-};
+var PersonaId = {
+
+    AUDIENCE: "https://privlyalpha.org:443",
+    EXPIRATION_LIMIT: (60 * 60 * 24 * 30), // 1 Month
+
+    /*
+     * Takes a User Certificate (identity cert), a secretkey, and a
+     * callback.
+     *
+     * The secret key is the user's not root certificate signer's
+     */
+    generateBackedIdentityAssertion: function(cert, secretkey, cb) {
+        var ISSUED_AT = new Date();
+        var EXPIRES_AT = new Date(new Date().valueOf() + this.EXPIRATION_LIMIT);
+        jwcrypto.assertion.sign({},
+          {expiresAt: EXPIRES_AT, issuedAt: ISSUED_AT,
+           audience: this.AUDIENCE},
+           secretkey,
+          function(err, assertion) {
+            cb(err, jwcrypto.cert.bundle(cert, assertion));
+          }
+        );
+    },
+
+    /**
+     * Create a payload to send to the directory provider.
+     *
+     * @param {pubkey} A PGP public key.
+     * @param {secretkey} A Persona secret key.
+     * @param {assertion} Backed Identity Assertion, which contains the
+     *                    public key associated with secret key.
+     **/
+    bundle: function(pubkey, secretkey, assertion, callback) {
+        self.sign(pubkey, secretkey, function(signature) {
+            callback({pgp: signature, bia: assertion});
+        });
+    },
+
+    /**
+     * Sign a PGP public key with a Persona secret key.
+     *
+     * @param {pubkey} A PGP public key.
+     * @param {secretkey} A Persona secret key.
+     **/
+    sign: function(pubkey, secretkey, callback) {
+        // jwcrypto.sign never returns an error so we can ignore it.
+        jwcrypto.sign({key: payload}, secretkey, function(err, signature) {
+            callback(signature);
+        });
+    },
+
+    /**
+     * Verify that a signature for a PGP key is correct, and extract the
+     * PGP key from the payload.
+     *
+     * @param {signedObject} The object returned from the 'sign'
+     *                       function.
+     * @param {pubkey} The Persona public key, whose pair was used to
+     *                 sign the signedObject.
+     **/
+    verify: function(signedObject, pubkey, callback) {
+        jwcrypto.verify(signedObject, pubkey, function(err, payload) {
+            callback(err, payload.key);
+        });
+    },
+
+    /**
+     * Verify that a directory provider payload contains a PGP public
+     * key signature that was signed by the public key contained within
+     * the backed identity assertion (bia) within the payload.
+     *
+     * @param {payload} The payload from a directory provider that
+     *                  contains a signed pgp key and bia.
+     * return "true" if valid passes, "false" otherwise.
+     **/
+    verifyPayload: function(payload) {
+        var bia_pubkey = extractPubkey(payload.bia);
+        self.verify(payload.pgp, bia_pubkey, function(err, pgp_pubkey) {
+            if (err !== null)
+                return true;
+        });
+        return false;
+    },
+
+    /**
+     * Extract a Persona public key from a backed identity assertion
+     * (bia).
+     *
+     * @param {bia} The backed identity assertion containing the public
+     *              key. This is assumed to be verified.
+     **/
+    extractPubkey: function(bia) {
+        var bundle = jwcypto.cert.unbundle(bia);
+        var assertion = bundle.signedAssertion;
+        // Assuming there is only ever one cert is a bad assumption, but
+        // it will hold for now.
+        var cert = bundle.certs[0];
+        
+        var pubkey_obj = jwcrypto.extractComponents(cert).pubkey;
+        var pubkey = jwcrypto.loadPublicKeyFromSimpleObject(pubkey_obj);
+
+        return pubkey;
+    },
+
+}
