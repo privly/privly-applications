064126f172dccc1fc63ea5f2c59f6544afa1f44a
Trevor Bramwell <trevor@bramwell.net>
2014-04-20 23:54:44 -0700

    Fix all the indenting, remove TODO in identity.js

--- pgp/js/identity.js
+++ pgp/js/identity.js
@@ -17,84 +17,82 @@ require('/lib/algs/ds');
 
 jwcrypto.addEntropy("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
 
-// TODO: Fix all indenting
 var PersonaId = {
-    /**
-     * Create a payload to send to the directory provider.
-     *
-     * @param {pubkey} A PGP public key.
-     * @param {secretkey} A Persona secret key.
-     * @param {assertion} Backed Identity Assertion, which contains the
-     *                    public key associated with secret key.
-     **/
-    bundle: function(pubkey, secretkey, assertion, callback) {
-        this.sign(pubkey, secretkey, function(err, signature) {
-            callback({pgp: signature, bia: assertion});
-        });
-    },
+  /**
+   * Create a payload to send to the directory provider.
+   *
+   * @param {pubkey} A PGP public key.
+   * @param {secretkey} A Persona secret key.
+   * @param {assertion} Backed Identity Assertion, which contains the
+   *                    public key associated with secret key.
+   **/
+  bundle: function(pubkey, secretkey, assertion, callback) {
+    this.sign(pubkey, secretkey, function(err, signature) {
+      callback({pgp: signature, bia: assertion});
+    });
+  },
 
-    /**
-     * Sign a PGP public key with a Persona secret key.
-     *
-     * @param {pubkey} A PGP public key.
-     * @param {secretkey} A Persona secret key.
-     **/
-    sign: function(pubkey, secretkey, callback) {
-        // jwcrypto.sign never returns an error so we can ignore it.
-        jwcrypto.sign({"key": pubkey}, secretkey, callback);
-    },
+  /**
+   * Sign a PGP public key with a Persona secret key.
+   *
+   * @param {pubkey} A PGP public key.
+   * @param {secretkey} A Persona secret key.
+   **/
+  sign: function(pubkey, secretkey, callback) {
+    // jwcrypto.sign never returns an error so we can ignore it.
+    jwcrypto.sign({"key": pubkey}, secretkey, callback);
+  },
 
-    /**
-     * Verify that a signature for a PGP key is correct, and extract the
-     * PGP key from the payload.
-     *
-     * @param {signedObject} The object returned from the 'sign'
-     *                       function.
-     * @param {pubkey} The Persona public key, whose pair was used to
-     *                 sign the signedObject.
-     **/
-    verify: function(signedObject, pubkey, callback) {
-        jwcrypto.verify(signedObject, pubkey, function(err, payload) {
-            callback(err, payload.key);
-        });
-    },
+  /**
+   * Verify that a signature for a PGP key is correct, and extract the
+   * PGP key from the payload.
+   *
+   * @param {signedObject} The object returned from the 'sign'
+   *                       function.
+   * @param {pubkey} The Persona public key, whose pair was used to
+   *                 sign the signedObject.
+   **/
+  verify: function(signedObject, pubkey, callback) {
+    jwcrypto.verify(signedObject, pubkey, function(err, payload) {
+      callback(err, payload.key);
+    });
+  },
 
-    /**
-     * Verify that a directory provider payload contains a PGP public
-     * key signature that was signed by the public key contained within
-     * the backed identity assertion (bia) within the payload.
-     *
-     * @param {payload} The payload from a directory provider that
-     *                  contains a signed pgp key and bia.
-     **/
-    verifyPayload: function(payload) {
-        var bia_pubkey = this.extractPubkey(payload.bia);
-        this.verify(payload.pgp, bia_pubkey, function(err, pgp_pubkey) {
-            if (err !== null)
-                return true;
-        });
-        return false;
-    },
+  /**
+   * Verify that a directory provider payload contains a PGP public
+   * key signature that was signed by the public key contained within
+   * the backed identity assertion (bia) within the payload.
+   *
+   * @param {payload} The payload from a directory provider that
+   *                  contains a signed pgp key and bia.
+   **/
+  verifyPayload: function(payload) {
+    var bia_pubkey = this.extractPubkey(payload.bia);
+    this.verify(payload.pgp, bia_pubkey, function(err, pgp_pubkey) {
+      if (err !== null)
+          return true;
+    });
+    return false;
+  },
 
-    /**
-     * Extract a Persona public key from a backed identity assertion
-     * (bia).
-     *
-     * @param {bia} The backed identity assertion containing the public
-     *              key. This is assumed to be verified.
-     **/
-    extractPubkey: function(bia) {
-        var bundle = jwcrypto.cert.unbundle(bia);
-        var assertion = bundle.signedAssertion;
-        // Assuming there is only ever one cert is a bad assumption, but
-        // it will hold for now.
-        // TODO: Verify Persona never uses multiple certs.
-        var cert = bundle.certs[0];
+  /**
+   * Extract a Persona public key from a backed identity assertion
+   * (bia).
+   *
+   * @param {bia} The backed identity assertion containing the public
+   *              key. This is assumed to be verified.
+   **/
+  extractPubkey: function(bia) {
+    var bundle = jwcrypto.cert.unbundle(bia);
+    var assertion = bundle.signedAssertion;
+    // Assuming there is only ever one cert is a bad assumption, but
+    // it will hold for now.
+    // TODO: Verify Persona never uses multiple certs.
+    var cert = bundle.certs[0];
 
-        var pubkey_obj = jwcrypto.extractComponents(cert).pubkey;
-        var pubkey = jwcrypto.loadPublicKeyFromObject(pubkey_obj);
+    var pubkey_obj = jwcrypto.extractComponents(cert).pubkey;
+    var pubkey = jwcrypto.loadPublicKeyFromObject(pubkey_obj);
 
-        return pubkey;
-    },
-
-}
+    return pubkey;
+  },
+};
