b0b7c984c3a877f7381ee326d8ab813482c3b083
irdan <daniel1555@gmail.com>
2014-05-01 15:28:57 -0700

    Refactoring keymanager functions

--- pgp/js/keymanager.js
+++ pgp/js/keymanager.js
@@ -3,21 +3,63 @@
  **/
 
 /**
- * The callbacks assign the state of the application.
+ * The functions of the keymanager
  *
- * This application can be placed into the following states:
- * 1. Pending Post: The user is properly logged in and can create content.
- * 2. genPGPkeys: The extension is checking PGP keys exist and are not expired.
+ * 1. setNewPGPKey: Update localforage key withe value passed in.
+ * 2. addNewPGPKey: Adds private/pub keys to localforage by calling set
+ * 3. promptUserToLogin: Ask user to login on directory provider, return when
+ *    persona key set
+ * 4. getPersonaKey: Get Persona Key from localstorage
+ * 5. genPGPkeys: The extension is checking PGP keys exist and are not expired.
+ * 6. uploadKey: Upload a backed identity assertion and public key to a
+ *    directory provider.
  */
 var keyManager = {
 
   /**
+   * This function takes a list of keys and returns the values retrieved from
+   * localforage.
+   */
+  getDatas: function(keys,callback) {
+    var datas = {
+      len: 0,
+      values: {}
+    };
+    localforage.setDriver('localStorageWrapper', function() {
+      for (var i = 0; i < keys.length; i++) {
+        localforage.getItem(keys[i], function(data) {
+          if (data != undefined) {
+            datas.values[keys[i]] = data;
+          } else {
+            console.log("Could not get", keys[i]);
+          }
+          datas.len += 1;
+          if (datas.len === keys.length){
+            callback(datas.values);
+          }
+        });
+      }
+    });
+  },
+
+  /**
+   * This function sets an item in localforage.
+   */
+  setDatas: function(key,value,callback){
+    localforage.setDriver('localStorageWrapper', function() {
+      localforage.setItem(key,value,function(result){
+        callback(result):
+      });
+    });
+  },
+
+  /**
    * Update the localforage key with the value passed in
    */
-  genPGPKeysHelper: function(key, appended_value,callback){
-    localforage.setDriver('localStorageWrapper',function(){
-      localforage.getItem('email',function(email){
-        localforage.getItem(key,function(value){
+  setNewPGPKey: function(key, appended_value, callback){
+    localforage.setDriver('localStorageWrapper', function() {
+      localforage.getItem('email', function(email) {
+        localforage.getItem(key, function(value) {
           if (value === null){
             value = {};
           }
@@ -38,12 +80,13 @@ var keyManager = {
    * Add pub keys to my_contacts
    * Add private/pub keypair to my_keypairs
    */
-  genPGPKeysAdder: function(keypair,callback){
-    callbacks.genPGPKeysHelper('my_keypairs',keypair,function(result){
-      keypair = keypair.publicKeyArmored;
-      callbacks.genPGPKeysHelper('my_contacts',keypair,function(result){
-        callback(result);
-      });
+  addNewPGPKey: function(keypair,callback){
+    var pubkey = keypair.publicKeyArmored;
+    keyManager.setNewPGPKey('my_keypairs',keypair,function(result){
+      keyManager.setNewPGPKey('my_contacts',pubkey,function(result){
+          callback(result);
+        }
+      );
     });
   },
 
@@ -65,18 +108,15 @@ var keyManager = {
    * Get Persona Key from localstorage
    */
   getPersonaKey: function(callback){
-    console.log("Getting PersonaKey");
     localforage.setDriver('localStorageWrapper',function(){
       localforage.getItem('persona-bridge',function(persona){
         localforage.getItem('email',function(email){
-          console.log(email);
-          console.log(persona);
           if (email == null){
             console.log("Email not found");
             callback(null);
           }
           if (persona != null) {
-            secretkey = PersonaId.getSecretKeyFromBridge(persona, email);
+            var secretkey = PersonaId.getSecretKeyFromBridge(persona, email);
             console.log("Persona secret key: ",secretkey);
             callback(secretkey);
           } else {
@@ -89,94 +129,91 @@ var keyManager = {
   },
 
   /**
-   * Genereate a PGP key if it does not exist or is nearly expired
+   * Evaluate if a new key needs to be generated.  
+   *
+   * This returns true under two conditions:
+   *   1) Localstorage does not contain a key.
+   *   2) Localstorage contains a key that is expired or is about to expire.
    */
-  genPGPKeys: function(){
+  needNewKey: function(callback){
     // Determine if a key is already in local storage
     localforage.setDriver('localStorageWrapper',function(){
-      localforage.getItem('email',function(email){
-        localforage.getItem('my_keypairs',function(keypairs){
-          if (keypairs !== null){ // it does not exist, make it
-            console.log("Generating New Key");
-            var workerProxy = new openpgp.AsyncProxy('../vendor/openpgp.worker.js');
-            workerProxy.seedRandom(10); // TODO: evaluate best value to use
-            workerProxy.generateKeyPair(
-              openpgp.enums.publicKey.rsa_encrypt_sign,
-              512,'username','passphrase',function(err,data){ // TODO: increase key
-                console.log("Key Generated");
-                callbacks.genPGPKeysAdder(data,function(result){
-                  console.log("Key added with adder");
-                  callbacks.uploadKey(email,function(outcome){
-                    if (outcome === false){
-                      console.log("Could not upload key.");
-                    } else {
-                      console.log("Upload success.");
-                    }
-                  });
-                });
-              }
-            );
-          } else { // it does exist,check expirey regenerate if needed
-            console.log("Already have a key, but uploading anyways.");
-            // TODO: check if key is about to expire and gen a new one if needed
-            callbacks.uploadKey(email,function(outcome){
-              if (outcome === false){
-                console.log("Could not upload key.");
-              } else {
-                console.log("Succesfully uploaded key.");
-              }
-            });
-          }
+      localforage.getItem('my_keypairs',function(keypairs){
+      if (keypairs === null){ // no key found, generate a new one
+        keyManager.genPGPKeys(function(outcome){
+          callback(outcome);
         });
-      });
+      } else { // it does exist,check expirey regenerate if needed
+        // TODO: check if key is about to expire and gen a new one if needed
+        // Note: Currently openPGP.js does not support setting a key
+        // expiration. Since nearly all of the keys we deal with are generated
+        // with openPGP.js, we do not yet check to see if keys expire.
+        
+        console.log("Already have a key.");
+        // This is just here to make dev easier remove me later
+        //console.log("Already have a key, but uploading anyways.");
+        //keyManager.uploadKey(function(outcome){
+          //callback(outcome);
+        //});
+      }
     });
   },
 
   /**
-   * Upload a backed identity assertion and public key to a directory provider.
-   * Currently this is not called because the remote resources this function
-   * depends on are not mature. 
+   * Generate a PGP key, add it to local storage, and upload it to directory.
+   */
+  genPGPKeys: function(){
+    console.log("Generating New Key");
+    var workerProxy = new openpgp.AsyncProxy('../vendor/openpgp.worker.js');
+    workerProxy.seedRandom(10); // TODO: evaluate best value to use
+    workerProxy.generateKeyPair(
+      openpgp.enums.publicKey.rsa_encrypt_sign,
+      512,'username','passphrase',function(err,data){ // TODO: increase key
+        keyManager.addNewPGPKey(data,function(result){
+          keyManager.uploadKey(function(outcome){});
+        });
+      }
+    );
+  },
+
+  
+  /**
+   * Upload a signed PGP public key to a directory provider.
    *
    * On a high level, this function needs to:
-   *   1) Generate a Backed Identity Assertion or have access to one
-   *   2) Take the public key out of the BIA and sign the key that was just
-   *      generated above in the genPGP function.
-   *   3) Upload the BIA and signed public key to the directory provider.
+   *   1) Have access to the Persona private key
+   *   2) Sign the key passed in with the Persona Private key
+   *   3) Upload the signed public key to the directory provider.
    */
-  uploadKey: function(email,callback){
+  uploadKey: function(callback){
     console.log("Uploading key");
     localforage.setDriver('localStorageWrapper',function(){
       localforage.getItem('my_keypairs',function(my_keys){
-        console.log("Got my_keyapirs ");
-        console.log(my_keys);
-        keypair = my_keys[email][0];// most recent key
-        console.log(keypair);
+      localforage.getItem('email',function(email){
+      localforage.getItem('directoryURL',function(directoryURL){
+        var keypair = my_keys[email][0];// most recent key
         if (keypair === null) {
           console.log("No key to upload found");
           callback(false);
         }
         var pubkey = keypair.publicKeyArmored;
-        callbacks.getPersonaKey(function(secretkey) {
-          console.log("Got PersonaKey");
-          console.log(secretkey);
+        keyManager.getPersonaKey(function(secretkey) {
           if (secretkey !== null) {
-            localforage.getItem('directoryURL',function(directoryURL){
-              directoryURL += "/store";
-              // TODO: Find a better way to seed jwcrypto.
-              jwcrypto.addEntropy("ACBpasdavbepOAEfBPBHESAEFGHA");
-              PersonaId.bundle(pubkey, secretkey, email, function(payload) {
-                console.log("payload:", payload);
-                $.get(
-                  directoryURL,
-                  payload
-                ).done(function(response){
-                  console.log("Upload Success");
-                  callback(true);
-                }
-                ).fail(function(response){
-                  console.log("Upload Fail");
-                  callback(false);
-                });
+            // TODO: Find a better way to seed jwcrypto.
+            jwcrypto.addEntropy("ACBpasdavbepOAEfBPBHESAEFGHA");
+            directoryURL += "/store";
+            PersonaId.bundle(pubkey, secretkey, email, function(payload) {
+              console.log("payload:", payload);
+              $.get(
+                directoryURL,
+                payload
+              ).done(function(response){
+                console.log("Upload Success");
+                callback(true);
+              }
+              ).fail(function(response){
+                console.log("Upload Fail");
+                callback(false);
               });
             });
           } else {
@@ -184,6 +221,8 @@ var keyManager = {
             callback(false);
           }
         });
+      });
+      });
       });
     });
   }
