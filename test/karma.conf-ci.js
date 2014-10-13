4b4273de9e6c7fbd2d880a4980272d529652b1fd
Sean McGregor <smcgregor08@cmc.edu>
2014-10-12 18:20:05 -0700

    removed non-Chrome and Firefox browsers from Continuous integration

--- test/karma.conf-ci.js
+++ test/karma.conf-ci.js
@@ -63,18 +63,6 @@ module.exports = function(config) {
       base: 'SauceLabs',
       browserName: 'firefox',
       version: '30'
-    },
-    'sl_ios_safari': {
-      base: 'SauceLabs',
-      browserName: 'iphone',
-      platform: 'OS X 10.9',
-      version: '7.1'
-    },
-    'sl_ie_11': {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      platform: 'Windows 8.1',
-      version: '11'
     }
   };
 
