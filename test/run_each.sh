4ba8520c4d7be0dbbed6d5852af67f8eb5eb97d5
Sean McGregor <smcgregor08@cmc.edu>
2014-10-22 21:29:20 -0700

    added karma.conf selection to parameters on run_each.sh

--- test/run_each.sh
+++ test/run_each.sh
@@ -1,7 +1,5 @@
 #
 # Runs each of the test sets defined below.
-# This script depends on the karma.conf.js file which defines many of the
-# options of the test.
 #
 echo "This script steps through a series of test sets that run via Karma."
 echo "Karma is a testing framework that will launch browsers and monitor files to re-run tests every time an edit is saved"
@@ -12,13 +10,23 @@ echo "export FILES_TO_TEST=YOUR_FILES_HERE"
 echo "Then you can issue 'karma start'"
 echo "!!!!!!"
 
+# Default to running the tests locally
+# If you want to run on Continuous Integration
+# then you can pass "karma.conf-ci.js" as the first positional argument.
+KARMA="karma.conf.js"
+if [ ! -z "$1" ]
+then
+  KARMA=$1
+fi
+
+# We need to report back a non-zero number if any of the tests failed
 declare -i ISFAIL=0
 
 runTest () {
   echo ""
   echo "running tests on shared libraries and $1"
   echo ""
-  export FILES_TO_TEST=$1 && karma start --single-run
+  export FILES_TO_TEST=$1 && karma start $KARMA --single-run
   ISFAIL=$(($ISFAIL|$?))
 }
 
@@ -32,12 +40,10 @@ runTest 'shared/javascripts/privly-web/show.js,PlainPost/js/show.js,PlainPost/te
 runTest 'shared/javascripts/privly-web/new.js,ZeroBin/js/base64.js,ZeroBin/js/rawdeflate.js,ZeroBin/js/rawinflate.js,ZeroBin/js/zerobin.js,ZeroBin/js/new.js,ZeroBin/test/new.js,ZeroBin/test/zerobin.js'
 runTest 'shared/javascripts/privly-web/show.js,ZeroBin/js/base64.js,ZeroBin/js/rawdeflate.js,ZeroBin/js/rawinflate.js,ZeroBin/js/zerobin.js,ZeroBin/js/show.js,ZeroBin/test/show.js,ZeroBin/test/zerobin.js'
 
-runTest
-
 if [ ! $ISFAIL -eq 0 ]
 then
   echo "You have some work to do: tests are failing"
-  exit -1
+  exit 1
 fi
 
 exit 0
