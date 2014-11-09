c2c5608ccb330f716c08d6227d9c23890e24b745
Sean McGregor <smcgregor08@cmc.edu>
2014-11-08 19:55:01 -0800

    fixed injection tests

--- test/selenium/specs/tc_show_injected.rb
+++ test/selenium/specs/tc_show_injected.rb
@@ -1,12 +1,6 @@
 # This test case checks whether the content at test.privly.org
 # injects into the host page properly. These specs should only
 # be run if the extension is loaded into the web browser.
-#
-# Note/todo: this could be significantly improved since it is only
-# checking whether well-formed links are being injected. In particular,
-# it is not checking wether non-whitelisted links are being injected or
-# whether misdirected links are being injected.
-#
 class TestInjected < Test::Unit::TestCase
 
   include Capybara::DSL # Provides for Webdriving
@@ -25,17 +19,29 @@ class TestInjected < Test::Unit::TestCase
     test_pages = [
       "http://test.privly.org/test_pages/whitelist.html",
       "http://test.privly.org/test_pages/url_parameters.html",
-      "http://test.privly.org/test_pages/nonwhitelist.html",
       "http://test.privly.org/test_pages/misdirection.html",
       "http://test.privly.org/test_pages/iframes.html"
     ]
-
     test_pages.each do |test_page|
       visit test_page
       links = page.all(:css, ".replace_link")
       links.each do |link|
-        link.find(:xpath, '../iframe')
+        assert link.has_xpath?('../iframe')
       end
+    end
+  end
+
+  def test_non_whitelisted
+
+    if not @@privly_extension_active
+      return
+    end
+
+    # Open these pages
+    test_pages = ["http://test.privly.org/test_pages/nonwhitelist.html"]
+    test_pages.each do |test_page|
+      visit test_page
+      assert page.has_no_xpath?('//iframe')
     end
   end
 
