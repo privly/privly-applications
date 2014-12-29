cb3ced9f70cf56f43159afb36d224d865e3a95d3
Sean McGregor <smcgregor08@cmc.edu>
2014-12-28 21:28:19 -0800

    made spec wait for element to appear

--- test/selenium/specs/tc_new.rb
+++ test/selenium/specs/tc_new.rb
@@ -49,7 +49,7 @@ class TestNew < Test::Unit::TestCase
       fill_in 'content', :with =>  "Hello WebDriver!"
       Selenium::WebDriver::Support::Select.new(page.driver.browser.find_element(:id, "seconds_until_burn")).select_by(:text, "1 Day")
       find_button("save").click
-      page.driver.browser.find_element(:css, "span.privlyUrl").click
+      page.find(:css,"span.privlyUrl").click
 
       # Open the created content in the "show" endpoint
       click_link("local_address")
