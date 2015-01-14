5da419e88ff03f20ac893eb3d4d4064ced2f35b7
Sean McGregor <smcgregor08@cmc.edu>
2015-01-14 14:22:35 -0800

    added unit test coverage to new modal box

--- History/test/modal.js
+++ History/test/modal.js
@@ -10,7 +10,16 @@
 describe ("History Application Modal Box Suite", function() {
 
  it("does not result in an error", function() {
-   
- });
+   var $reference = jQuery('<div/>', {
+       'data-canonical-href': 'https://priv.ly/posts/2?privlyApp=PlainPost#privlyInject1',
+       'class': 'btn btn-default preview_link'
+   });
+   historyModal.eventHandlers.iframeReturn($reference);
+   historyModal.eventHandlers.prevPreview();
+   historyModal.eventHandlers.nextPreview();
+   historyModal.eventHandlers.destroyLink();
 
+   privlyNetworkService.sameOriginDeleteRequest = function(){};
+   historyModal.eventHandlers.confirmDestruction();
+ });
 });
