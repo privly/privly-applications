aa4f6466c28dea89993b80a2271f1b633ec312f4
Daniel Reichert <daniel1555@gmail.com>
2015-03-02 13:20:25 -0800

    Add js listeners and functions for registration
    Note that many of these functions were taken directly from the
    Login/js/new.js file. Only the pendingRegistration and
    registrationFailure functions were modified.

--- Pages/js/first_run.js
+++ Pages/js/first_run.js
@@ -8,6 +8,47 @@
  * Initialize the applications by showing and hiding the proper
  * elements.
  */
+var callbacks = {
+
+  /**
+   * Submit the registration form and await the return of the registration.
+   */
+  submitRegistration: function() {
+    privlyNetworkService.sameOriginPostRequest(
+      privlyNetworkService.contentServerDomain() + "/users/invitation",
+      callbacks.checkRegistration,
+      { "user[email]":  $("#register_email").val() });
+  },
+
+  /**
+   * Check to see if the user's registration was accepted by the server.
+   */
+  checkRegistration: function(response) {
+     if ( response.json.success === true ) {
+      callbacks.pendingRegistration();
+    } else {
+      callbacks.registrationFailure();
+    }
+  },
+
+  /**
+   * Tell the user their registration was submitted.
+   */
+  pendingRegistration: function() {
+    $("#register_feedback").html("<strong>Thanks!</strong> You should receive an email soon!");
+    $("#register_feedback").show();
+    $("#registration-form").delay( 1500 ).animate({"margin-top": "-140px", }, 800);
+  },
+
+  /**
+   * Tell the user their registration was rejected.
+   */
+  registrationFailure: function() {
+    $("#register_feedback").text("<strong>Error</strong> communicating with registration server.");
+    $("#register_feedback").show();
+  }
+
+}
 function init() {
 
   // Set the nav bar to the proper domain
@@ -21,6 +62,14 @@ function init() {
 
   $("#messages").hide();
   $("#form").show();
+  $("#registration-form").hide();
+  $("#content_server_btn").text(ls.getItem("posting_content_server_url").split("//")[1]);
+
+  document.querySelector("#registration").addEventListener('click', function(){
+    $("#registration-form").show();
+  });
+
+  document.querySelector("#register_btn").addEventListener('click', callbacks.submitRegistration);
 
   // Show a preview of the tooltip to the user
   var glyphHTML = privlyTooltip.glyphHTML();
