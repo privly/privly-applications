898f1acf9c18ce77b7c30a0489fc6fb7c27dc643
Sean McGregor <smcgregor08@cmc.edu>
2015-03-06 00:17:21 -0800

    removed old test code

--- SplitImage/test/show.js
+++ SplitImage/test/show.js
@@ -6,9 +6,7 @@
 describe ("SplitImaage Show Suite", function() {
   
   it("initializes properly", function() {
-    var domain = privlyNetworkService.contentServerDomain();
-    expect(domain).toBe($(".home_domain").attr("href"));
-    expect(domain.split("/")[2]).toBe($(".home_domain").text());
+    // pass
   });
   
 });
