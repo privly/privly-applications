c637af12ae5075c5ebc13d10b0521af22c7afef1
Sean McGregor <smcgregor08@cmc.edu>
2015-05-21 20:41:55 -0700

    removed html2js preprocessor from Karma

--- test/karma.conf-ci.js
+++ test/karma.conf-ci.js
@@ -27,9 +27,6 @@ module.exports = function(config) {
   // Javascripts.
   var filesToTest = [
 
-    // HTML files to use as a fixtures
-    '*/*.html',
-
     // Force jquery to load first since it is a dependency
     'vendor/jquery.min.js',
 
@@ -77,13 +74,10 @@ module.exports = function(config) {
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
 
-    // Provide the HTML document as a fixture
+    // Provide coverage information for the shared libraries
     preprocessors: {
-          '*/*.html': ['html2js'],
-
-          // Load all the shared libraries at the top level
-          'shared/javascripts/*.js': 'coverage'
-        },
+      'shared/javascripts/*.js': 'coverage'
+    },
 
     // list of files / patterns to load in the browser
     files: filesToTest,
