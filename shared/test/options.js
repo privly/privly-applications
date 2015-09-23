8310dce5fc33289394b099ac42d6a4f1cd0523a7
Sean McGregor <smcgregor08@cmc.edu>
2015-09-23 11:00:32 -0700

    added test coverage

--- shared/test/options.js
+++ shared/test/options.js
@@ -6,6 +6,26 @@
 /*global Privly, ls */
 describe('Privly.options', function () {
 
+  beforeEach(function() {
+    var domIDs = [
+      {id: "server_status", type: "div"},
+      {id: "user", type: "div"},
+      {id: "content_server_url", type: "div"},
+      {id: "other_content_server", type: "div"},
+      {id: "server_form", type: "div"}
+    ];
+    domIDs.forEach(function(ob){
+      var newElement = $('<' + ob.type + '/>', {
+        id: ob.id
+      });
+      $(document.body).append(newElement);
+    });
+  });
+
+  afterEach(function() {
+    document.body.innerHTML = "";
+  });
+
   it('isDomainValid() should validate FQDN', function () {
     // Valid domains that should pass
     expect(Privly.options.isDomainValid('localhost')).toBe(true);
@@ -236,6 +256,44 @@ describe('Privly.options', function () {
     expect(function () { Privly.options.setGlyph({color: false, cells: []}); }).toThrow(new Error('invalid argument'));
     expect(function () { Privly.options.setGlyph({color: [], cells: []}); }).toThrow(new Error('invalid argument'));
     expect(function () { Privly.options.setGlyph({color: '123123', cells: null}); }).toThrow(new Error('invalid argument'));
+  });
+
+  it('saves the server', function () {
+    var ev = {target: {value: ""}};
+    saveServer(ev);
+    expect($("#user").css("display")).toBe("none");
+    ev.target.value = "save_server";
+    var server = document.getElementById("content_server_url");
+    server.value = "other";
+    var sham = "https://sham.com";
+    document.getElementById("other_content_server").value = sham;
+    saveServer(ev);
+    var cur = Privly.options.getServerUrl();
+    expect(cur).toBe(sham);
+    server.value = "alpha";
+    saveServer(ev);
+    cur = Privly.options.getServerUrl();
+    expect(cur).toBe("https://privlyalpha.org");
+    server.value = "dev";
+    saveServer(ev);
+    cur = Privly.options.getServerUrl();
+    expect(cur).toBe("https://dev.privly.org");
+    server.value = "local";
+    saveServer(ev);
+    cur = Privly.options.getServerUrl();
+    expect(cur).toBe("http://localhost:3000");
+  });
+
+  it('does not result in an error when restoring the server', function () {
+    // todo, refactor the script being tested
+    Privly.options.setServerUrl("https://privlyalpha.org");
+    restoreServer();
+    Privly.options.setServerUrl("https://dev.privly.org");
+    restoreServer();
+    Privly.options.setServerUrl("http://localhost:3000");
+    restoreServer();
+    Privly.options.setServerUrl("http://other.com");
+    restoreServer();
   });
 
 });
